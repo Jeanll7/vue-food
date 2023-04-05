b0ce38a249648612271e03185ba0b2dee7daa1a5
devjean <jeancarloleall@gmail.com>
2023-04-05 01:51:45 -0300

    input-form

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>Componente de mensagem</p>
+    <div>
+      <form id="burger-form">
+        <div class="input-container">
+          <label for="nome">Nome do cliente:</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            v-model="nome"
+            placeholder="Digite o seu nome"
+          />
+        </div>
+        <div class="input-container">
+          <label for="pao">Ecolha o pão:</label>
+          <select name="pao" id="pao" v-model="pao">
+            <option value="">Selecione o seu pão</option>
+            <option value="integral">Integral</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BurgerForm",
+};
+</script>
+
+<style scoped></style>
