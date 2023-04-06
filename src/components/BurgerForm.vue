a40b5d40744e1af3dec13dae5437d8d430b58141
devjean <jeancarloleall@gmail.com>
2023-04-06 00:51:58 -0300

    backend

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -27,8 +27,19 @@
             <option value="maminha">Maminha</option>
           </select>
         </div>
-        <div class="input-container">
-          <label for="opcionais">Selecione os opcionais:</label>
+        <div id="opcionais-container" class="input-container">
+          <label id="opcionais-title" for="opcionais"
+            >Selecione os opcionais:</label
+          >
+          <div class="checkbox-container">
+            <input
+              type="checkbox"
+              name="opcionais"
+              v-model="opcionais"
+              value="salame"
+            />
+            <span>Salame</span>
+          </div>
           <div class="checkbox-container">
             <input
               type="checkbox"
@@ -50,7 +61,91 @@
 <script>
 export default {
   name: "BurgerForm",
+  data() {
+    return {
+      paes: null,
+      carnes: null,
+      opcionaisdata: null,
+      nome: null,
+      carne: null,
+      opcionais: [],
+      status: "Solicitado",
+      msg: null,
+    };
+  },
+  methods: {
+    async getIngredientes() {
+      const req = await fetch("http://localhost:3000/ingredientes");
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #fcba03;
+}
+
+input,
+select {
+  padding: 5px 10px;
+  width: 300px;
+}
+#opcionais-container {
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+#opcionais-title {
+  widows: 100%;
+}
+
+.checkbox-container {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
+}
+
+.checkbox-container span,
+.checkbox-container input {
+  width: auto;
+}
+
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
+}
+
+.submit-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  /* margin: 0 auto; */
+  margin-left: 15px;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
