72574787422385c7de1e8834e22198971bf75a39
devjean <jeancarloleall@gmail.com>
2023-04-08 10:18:57 -0300

    dashboard table

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -47,7 +47,25 @@
       </div>
       <div>
         <select name="status" class="status">
-          <option value="">Selecione o statue do Hamburger</option>
+          <option value="">Selecione</option>
+        </select>
+        <button class="delete-btn">Cancelar</button>
+      </div>
+    </div>
+    <div class="burger-table-row">
+      <div class="order-number">1</div>
+      <div>João</div>
+      <div>Pao xis</div>
+      <div>Carne</div>
+      <div>
+        <ul>
+          <li>Salame</li>
+          <li>Tomate</li>
+        </ul>
+      </div>
+      <div>
+        <select name="status" class="status">
+          <option value="">Selecione</option>
         </select>
         <button class="delete-btn">Cancelar</button>
       </div>
@@ -65,5 +83,55 @@ export default {
 #burger-table {
   max-width: 1200px;
   margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+}
+
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
 }
 </style>
