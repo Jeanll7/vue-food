e3de3b177735d1802d6cae44ddc96c45b79a7c3e
devjean <jeancarloleall@gmail.com>
2023-04-08 10:33:24 -0300

    delete banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,16 +7,12 @@
 </template>
 
 <script>
-import Banner from "../components/Banner";
 import BurgerForm from "../components/BurgerForm";
 
 export default {
   name: "Home",
   components: {
-    Banner,
     BurgerForm,
   },
 };
 </script>
-
-// 3:19 #23
